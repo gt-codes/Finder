93c3e5d0d99765b7429f5eff0b1450d22747da73
Garrett Tolbert <garrett1.tolbert@gmail.com>
2020-02-27 23:09:31 -0500

    changed main UI

--- client/finder/src/components/FriendsList.js
+++ client/finder/src/components/FriendsList.js
@@ -2,19 +2,19 @@ import React, {useState,useEffect} from 'react';
 import './styles/FriendsList.css';
 import { graphql } from 'react-apollo'
 import {getFriendsQuery} from '../queries'
+import avi from '../resources/defaultAvi.png'
 
 const Friend = (props) => {
     const {data} = props;
     return (
-        <div className='fn-container' onClick={() => {props.openFriend(true);props.selectFriend(data.graphqlID)}}>
-            <img src={'https://i.pravatar.cc/75'} alt='avatar'/>
-            <div className='fn-md'>
-                <div className='fn-md-tr'>
-                    <h3>{data.name}</h3>
-                    <h4>{data.city}, {data.state}</h4>
-                </div>
-                <p>{data.notes}</p>
-            </div>
+        <div className='fn-container' 
+            onClick={() => {
+                props.openFriend(true);
+                props.selectFriend(data.graphqlID)
+            }}
+        >
+            <img src={avi} alt='avatar'/>
+            <h3>{data.name}</h3>
         </div>
     );
 }
